72380331adf95d4145e1b39d04d3b0e3df894bf1
ItayShuk <itay.shukrun@mail.huji.ac.il>
2020-10-23 16:14:11 +0300

    add sub push and pop updated

--- CodeWriter.py
+++ CodeWriter.py
@@ -13,16 +13,16 @@ class CodeWriter:
 
     def writeOrder(self, order, output):
         switch = {
-            "push": self.pushCommand(order, output),
-            "pop": self.popCommand(order, output),
-            "add": self.add(order, output),
-            "sub": self.sub(order, output)
+            "push": self.pushCommand(order[1], order[2], output),
+            "pop": self.popCommand(order[1], order[2], output),
+            "add": self.add(output),
+            "sub": self.sub(output)
         }
         switch.get(order[0])
 
-    def pushCommand(self, order, output):
-        if order[1] == "constant":
-            output.write("@" + order[2] + "\n")
+    def pushCommand(self, variable, index, output):
+        if variable == "constant":
+            output.write("@" + index + "\n")
             output.write("D=A\n")
             output.write("@SP\n")
             output.write("A=M\n")
@@ -30,9 +30,9 @@ class CodeWriter:
             output.write("@SP\n")
             output.write("M=M+1\n")
         else:
-            output.write("@" + order[2] + "\n")
+            output.write("@" + index + "\n")
             output.write("D=A\n")
-            output.write("@" + self.segmentsCodes[order[1]] + "\n")
+            output.write("@" + self.segmentsCodes[variable] + "\n")
             output.write("A=A+D\n")
             output.write("D=M\n")
             output.write("@SP\n")
@@ -41,10 +41,10 @@ class CodeWriter:
             output.write("@SP\n")
             output.write("M=M+1\n")
 
-    def popCommand(self, order, output):
-        output.write("@" + order[2] + "\n")
+    def popCommand(self, variable, index, output):
+        output.write("@" + index + "\n")
         output.write("D=A\n")
-        output.write("@" + self.segmentsCodes[order[1]] + "\n")
+        output.write("@" + self.segmentsCodes[variable] + "\n")
         output.write("D=A+D\n")
         output.write("@" + self.segmentsCodes["temp"] + "\n")
         output.write("M=D\n")
@@ -55,3 +55,20 @@ class CodeWriter:
         output.write("@" + self.segmentsCodes["temp"] + "\n")
         output.write("A=M\n")
         output.write("M=D\n")
+
+    def add(self, output):
+        self.preAddSub(output)
+        output.write("M=D+M" + "\n")
+        self.pushCommand("temp", "2", output)
+
+    def sub(self, output):
+        self.preAddSub(output)
+        output.write("M=D-M" + "\n")
+        self.pushCommand("temp", "2", output)
+
+    def preAddSub(self, output):
+        self.popCommand("temp", "1", output)
+        self.popCommand("temp", "2", output)
+        output.write("@" + str(int(self.segmentsCodes["temp"]) + 1) + "\n")
+        output.write("D=M")
+        output.write("@" + str(int(self.segmentsCodes["temp"]) + 2) + "\n")
