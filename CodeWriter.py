d7a432a31fb6fa7828f7c769af41ddd7d287920d
ItayShuk <itay.shukrun@mail.huji.ac.il>
2020-10-23 14:47:36 +0300

    Parser update

--- CodeWriter.py
+++ CodeWriter.py
@@ -1,8 +1,40 @@
 class CodeWriter:
+    RAM = []
+    memoryPointers = [("SP", 256),
+                      ("local", 300),
+                      ("argument", 400),
+                      ("temp", 5),
+                      ("this", 3000),
+                      ("that", 3010),
+                      ("pointer", 500),
+                      ("static", 16)
+                      ]
+
+    SP = 0
+
+    segmentsCodes = {"local": "LCL",
+                     "argument": "ARG",
+                     "this": "THIS",
+                     "that": "THAT",
+                     "pointer": "3",
+                     "temp": "5"}
 
     def __init__(self, fileOrders, fileDest):
         output = open(fileDest[:-2] + "asm")
         for order in fileOrders:
-            writeOrder(order, output)
+            self.writeOrder(order, output)
 
     def writeOrder(self, order, output):
+        switch = {
+            "push": self.pushCommand(order, output),
+            "pop": self.popCommand(order, output),
+            "add": self.add(order, output),
+            "sub": self.sub(order, output)
+        }
+        switch.get(order[0])
+
+    def pushCommand(self, order, output):
+        output.write("@" + order[2]+"\n")
+        output.write("D=A\n")
+        output.write("@")
+
